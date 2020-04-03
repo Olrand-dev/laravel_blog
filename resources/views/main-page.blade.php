53bc4f1f8bd565cc7062605487c3a0bc6dfe757c
olrand <olrand.dev@gmail.com>
2020-04-03 20:33:54 +0300

    main ui elements design

--- resources/views/main-page.blade.php
+++ resources/views/main-page.blade.php
@@ -35,6 +35,53 @@
                 magni dolore quae ipsam repudiandae cumque neque?
             </p>
 
+
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputEmail4">Email</label>
+                    <input type="email" class="form-control" id="inputEmail4">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="inputPassword4">Password</label>
+                    <input type="password" class="form-control" id="inputPassword4">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="inputAddress">Address</label>
+                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+            </div>
+            <div class="form-group">
+                <label for="inputAddress2">Address 2</label>
+                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputCity">City</label>
+                    <input type="text" class="form-control" id="inputCity">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="inputState">State</label>
+                    <select id="inputState" class="form-control">
+                        <option selected>Choose...</option>
+                        <option>...</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="inputZip">Zip</label>
+                    <input type="text" class="form-control" id="inputZip">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-check">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="customCheck1">
+                        <label class="custom-control-label" for="customCheck1">Check this custom checkbox</label>
+                      </div>
+                </div>
+            </div>
+
+
+
             <div class="content-collection d-flex justify-content-between">
 
                 <div class="card col-md-3">
@@ -169,7 +216,22 @@
                     
                 </ul>
             </nav>
-            
+
+            <div class="box-header">
+                Теги
+            </div>
+
+            <div class="tags-box">
+                <a href="#" class="tag">tag 1 (5)</a>
+                <a href="#" class="tag">tag 2 (5)</a>
+                <a href="#" class="tag">tag 3 (5)</a>
+                <a href="#" class="tag">tag 4 (5)</a>
+                <a href="#" class="tag">tag 5 (5)</a>
+                <a href="#" class="tag">tag 6 (5)</a>
+                <a href="#" class="tag">ndjndf jdfnkjd (5)</a>
+                <a href="#" class="tag">djgndgjdj (5)</a>
+                <a href="#" class="tag">jhbdgbdfj jfbd dfjnj (5)</a>
+            </div>
 
         </div>
 

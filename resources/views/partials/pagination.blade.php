1a4419ca94edfc0c230fe72dd17353eed81d8621
olrand <olrand.dev@gmail.com>
2020-04-25 19:56:22 +0300

    pagination mobile

--- resources/views/partials/pagination.blade.php
+++ resources/views/partials/pagination.blade.php
@@ -1,15 +1,10 @@
 @if ($paginator->hasPages())
 
     <nav class="d-flex justify-content-center">
+
         <ul class="pagination">
 
             @if ($paginator->onFirstPage())
-
-                <li class="page-item mobile disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
-                    <span class="page-link" aria-hidden="true">
-                        <i class="fas fa-angle-left"></i> Пред. страница
-                    </span>
-                </li>
 
                 <li class="page-item disabled" aria-disabled="true">
                     <span class="page-link" aria-hidden="true">
@@ -23,12 +18,6 @@
                 </li>
 
             @else
-
-                <li class="page-item mobile">
-                    <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">
-                        <i class="fas fa-angle-left"></i> Пред. страница
-                    </span>
-                </li>
 
                 <li class="page-item">
                     <a class="page-link" href="{{ $paginator->url(1) }}">
@@ -82,12 +71,6 @@
                     </a>
                 </li>
 
-                <li class="page-item mobile">
-                    <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">
-                        След. страница <i class="fas fa-angle-right"></i>
-                    </a>
-                </li>
-
             @else
 
                 <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
@@ -101,15 +84,25 @@
                     </span>
                 </li>
 
-                <li class="page-item mobile disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
-                    <span class="page-link" aria-hidden="true">
-                        След. страница <i class="fas fa-angle-right"></i>
-                    </span>
-                </li>
-
             @endif
 
         </ul>
+
+        <ul class="pagination mobile">
+
+            <li class="page-item @if ($paginator->currentPage() === 1) disabled @endif">
+                <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">
+                    <i class="fas fa-angle-left"></i> Пред. страница
+                </a>
+            </li>
+            <li class="page-item @if ($paginator->currentPage() === $paginator->lastPage()) disabled @endif">
+                <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">
+                    <i class="fas fa-angle-right"></i> След. страница
+                </a>
+            </li>
+
+        </ul>
+
     </nav>
     
 @endif
